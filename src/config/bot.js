1027c9bd94eff1492e920375daea4b644d8fdc46
yakoub97 <fadyyakoub0326@gmail.com>
2026-06-25 13:41:58 -0700

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "WeMadeSro Online ⚔ System D11 + Job + Unique + Ranks + Hourly Ranking Events and More!",
+        name: "AlexusSro Online ⚔ System D11 + Job + Unique + Ranks + Hourly Ranking Events and More!",
         // Activity type number (0 = Playing).
         type: 0,
       },
